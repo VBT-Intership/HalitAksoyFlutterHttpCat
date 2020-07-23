1e91abe1293589d9dd0e4c1636fd742add56033b
Halit Aksoy <jhalitaksoy@gmail.com>
2020-07-24 02:33:39 +0300

    Added goto http.cat button and url_launcher plugin

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_http_cat/ui/components/http_cat_list.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -24,10 +25,24 @@ class _HomeScreenState extends State<HomeScreen> {
   AppBar buildAppBar() {
     return AppBar(
       backgroundColor: Colors.white,
+      actions: <Widget>[
+        FlatButton(onPressed: () {
+          _launchURL();
+        }, child: Text("Goto http.cat"))
+      ],
       title: Text(
         "Http Cats",
         style: TextStyle(color: Colors.red),
       ),
     );
+  }
+
+  _launchURL() async {
+    const url = 'https://http.cat/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      //throw 'Could not launch $url';
+    }
   }
 }
